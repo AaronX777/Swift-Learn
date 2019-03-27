887a345a64470751670762b2a3e29812c4df218f
AaronX777 <itisgo@gmail.com>
2019-03-27 15:49:52 +0800

    create Core Data, change Data Mod

---
+++ FoodPin v19/FoodPin/Controller/MapViewController.swift
@@ -0,0 +1,86 @@
+//
+//  MapViewController.swift
+//  FoodPin
+//
+//  Created by rd on 2019/3/15.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+import MapKit
+
+import UIKit
+
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Do any additional setup after loading the view.
+//    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBOutlet var mapView: MKMapView!
+    var restaurant = Restaurant()
+
+    override func viewDidLoad() {
+        
+        
+        super.viewDidLoad()
+        
+        mapView.delegate = self
+        mapView.showsCompass = true
+        mapView.showsScale = true
+        mapView.showsTraffic = true
+        
+        let geoColer = CLGeocoder()
+        geoColer.geocodeAddressString(restaurant.location, completionHandler: { placemarks, error in if let error = error{
+            print(error)
+            return
+            }
+            if let placemarks = placemarks {
+                let placemarks = placemarks[0]
+                let annotation = MKPointAnnotation()
+                annotation.title = self.restaurant.name
+                annotation.subtitle = self.restaurant.type
+                
+                if let location = placemarks.location {
+                    annotation.coordinate = location.coordinate
+                    
+                    self.mapView.showAnnotations([annotation], animated: true)
+                    self.mapView.selectAnnotation(annotation, animated: true)
+                }
+            }
+        })
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let identifier = "MyMarker"
+
+        if annotation.isKind(of: MKUserLocation.self) {
+            return nil
+        }
+
+        //
+        var annotationView: MKMarkerAnnotationView? = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView
+
+        if annotationView == nil {
+            annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+
+        }
+        annotationView?.glyphText = "😀"
+        annotationView?.markerTintColor = UIColor.orange
+
+        return annotationView
+    }
+    
+
+}
---
+++ FoodPin v19/FoodPin/Extensions/UIColor+Ext.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor+Ext.swift
+//  FoodPin
+//
+//  Created by rd on 2019/3/14.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import Foundation
+
+
+import UIKit
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        let redValue = CGFloat(red) / 255.0
+        let greenValue = CGFloat(green) / 255.0
+        let blueValue = CGFloat(blue) / 255.0
+        self.init(red: redValue, green: greenValue, blue: blueValue, alpha: 1.0)
+    }
+}
